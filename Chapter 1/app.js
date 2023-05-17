144fe77a7ae5ea7d967e2528602dd886a3af888f
christoffelk <emperorchrisz@gmail.com>
2023-05-17 15:17:29 +0700

    feat: chapter 1-7 event handling on vue js

--- Chapter 1/app.js
+++ Chapter 1/app.js
@@ -1,40 +1,21 @@
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//         maximum: 50,
-//         products: null,
-//         cart: []
-//     },
-//     mounted: function() {
-//         fetch('https://hplussport.com/api/products/order/price')
-//         .then(response => response.json())
-//         .then(data => {
-//             this.products = data;
-//         });
-//     },
-//     methods: {
-//         addItem: function (product) {
-//             this.cart.push(product);
-//         }
-//     }
-// });
-
 const app = new Vue({
-    el: "#app",
-    data:{
+    el: '#app',
+    data: {
         maximum: 50,
-        product: null
+        products: null,
+        cart: []
     },
-    mounted:  async function() {
-        // const response = await fetch('https://hplussport.com/api/products/order/price')
-
-        // const result =await  response.json();
-
-        // this.product = result.data
-
+    mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
         .then(response => response.json())
-        .then(data => {this.product = data})
-
+        .then(data => {
+            this.products = data;
+        });
+    },
+    methods: {
+        addItem: function (product) {
+            this.cart.push(product);
+        }
     }
-  });
+});
+
