162b22362440b9963cbec33129c4b270bc6f3965
christoffelk <emperorchrisz@gmail.com>
2023-05-17 14:37:29 +0700

    feat: practice chapter 1-2 add image using v-bind

--- Chapter 1/app.js
+++ Chapter 1/app.js
@@ -1,20 +1,30 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        maximum: 50,
-        products: null,
-        cart: []
-    },
-    mounted: function() {
-        fetch('https://hplussport.com/api/products/order/price')
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        });
-    },
-    methods: {
-        addItem: function (product) {
-            this.cart.push(product);
-        }
-    }
-});
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         maximum: 50,
+//         products: null,
+//         cart: []
+//     },
+//     mounted: function() {
+//         fetch('https://hplussport.com/api/products/order/price')
+//         .then(response => response.json())
+//         .then(data => {
+//             this.products = data;
+//         });
+//     },
+//     methods: {
+//         addItem: function (product) {
+//             this.cart.push(product);
+//         }
+//     }
+// });
+const data = {
+    "name": "hello world",
+    "description": "hello wolrd",
+    price: "34000",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4xAZC_IEqYENTyekCJq6SViwmesoEN_lsUiozV-pl&s"
+}
+const app = new Vue({
+    el: "#app",
+    data
+  });
